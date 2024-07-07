6a5e65c50a5b9fc73c5a34c9bf22acf6e9903d90
arminwitte <110226001+arminwitte@users.noreply.github.com>
2024-07-07 13:12:19 +0200

    Add files via upload

---
+++ streamlit_app.py
@@ -0,0 +1,42 @@
+# streamlit_app.py
+
+import streamlit as st
+import datetime
+
+def check_password():
+    """Returns `True` if the user had the correct password."""
+
+    def password_entered():
+        """Checks whether a password entered by the user is correct."""
+        if st.session_state["password"] == st.secrets["password"]:
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]  # don't store password
+        else:
+            st.session_state["password_correct"] = False
+
+    if "password_correct" not in st.session_state:
+        # First run, show input for password.
+        st.text_input(
+            "Password", type="password", on_change=password_entered, key="password"
+        )
+        return False
+    elif not st.session_state["password_correct"]:
+        # Password not correct, show input + error.
+        st.text_input(
+            "Password", type="password", on_change=password_entered, key="password"
+        )
+        st.error("😕 Password incorrect")
+        return False
+    else:
+        # Password correct.
+        return True
+
+if check_password():
+    d = {}
+    d[datetime.date(year=2023,month=1,day=26)] = "Ich liebe dich"
+    date = st.date_input("Tag wählen")
+    s = d.get(date)
+    if s:
+        st.write(s)
+    else:
+        st.warning("Keine Botschaft")
