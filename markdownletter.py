9df35944eb01bf2f908160ba3ffb0df6caf7028e
arminwitte <110226001+arminwitte@users.noreply.github.com>
2024-08-28 00:21:30 +0200

    Update and rename mdletter.py to markdownletter.py

--- markdownletter.py
+++ markdownletter.py
@@ -7,7 +7,7 @@ from markdown.extensions.extra import ExtraExtension
 from markdown.extensions.codehilite import CodeHiliteExtension
 from markdown.extensions.toc import TocExtension
 
-class DIN5002Letter:
+class MarkdownLetter:
     def __init__(self, recipient_address):
         self.recipient_address = recipient_address
         self.html_template = """
